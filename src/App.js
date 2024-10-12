256ac9384fa846b409c4b35756c33aa05534f53d
MAHESH <maheshshinde9100@gmail.com>
2024-10-12 22:19:38 +0530

    Content showing issue solved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import FirebaseContext from "./context/firebase";
-function App() {
-    console.log("context",FirebaseContext);
-    console.log(FirebaseContext);
-    <div>Hello</div>
-  }
+// import FirebaseContext from "./context/firebase";
+// function App() {
+//     console.log("context",FirebaseContext);
+//     console.log(FirebaseContext);
+//     <div>Hello</div>
+//   }
+// export default App;
 
+
+import React, { useContext } from 'react';
+import FirebaseContext from "./context/firebase";
+
+function App() {
+    const firebaseContext = useContext(FirebaseContext); 
+    console.log("context", firebaseContext);  
+    return (
+        <div>Hello</div>  
+    );
+}
 export default App;
