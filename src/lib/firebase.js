4ca5c4d6bc35a5bf8b595df4a977d3b9b9553606
MAHESH <maheshshinde9100@gmail.com>
2024-10-19 21:10:04 +0530

    removed unwanted comment in firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,6 @@
 import Firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
-// import { seedDatabase } from '../seed'; 
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const mail = process.env.REACT_APP_FIREBASE_EMAIL;
-// const pwd = process.env.REACT_APP_FIREBASE_PASSWORD;
-
 const config = {
     apiKey: "AIzaSyCEjLHqLpW5Prv-5zhb33RUhz6Z1RwrhXc",
     authDomain: "instagram-3a3d7.firebaseapp.com",
@@ -17,18 +10,6 @@ const config = {
     messagingSenderId: "132423561098",
     appId: "1:132423561098:web:6e4ce7ed6d4f37c1d86273"
 };
-
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
-
-// // Function to sign in and seed the database
-// firebase.auth().signInWithEmailAndPassword(mail,pwd)
-//   .then(() => {
-//     console.log('Authentication successful');
-//     seedDatabase(firebase);  // Seed the database
-//   })
-//   .catch(error => {
-//     console.error("Authentication failed:", error.message);
-//   });
-
 export { firebase, FieldValue };
