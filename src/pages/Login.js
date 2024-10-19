634a0e5acc8e0b6ac7e415809f5770939f69e65a
MAHESH <maheshshinde9100@gmail.com>
2024-10-19 20:25:28 +0530

    worked on login page 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React,{useContext, useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const  { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const isInvalid = password ==="" || emailAddress==="";
+    const handleLogin=() =>{};
+
+    useEffect(()=>{
+        document.title = "Login - Instagram";
+    },[]);
     return (
         <div className='flex justify-center items-center h-screen'>
           Login Page!
