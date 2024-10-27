c83979ebc2cbf37f85bad5df7b202873bdc58796
MAHESH <maheshshinde9100@gmail.com>
2024-10-27 21:20:09 +0530

    use-auth-listener.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,12 @@ function App() {
         <Router>
             <Suspense fallback={<p>Loading...</p>}>
                 <Routes>
-                    {/* <Route path={'/'} element={
-                        <h1 className="text-3xl font-bold underline">
-                            Hello world!
-                        </h1>} /> */}
                     <Route path={ROUTES.LOGIN} element={<Login />} />
 
                     <Route path={ROUTES.SIGN_UP} element={<Signup />} />
 
                     <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-                    
+
                     <Route path='*' element={<NotFound />} />
                 </Routes>
             </Suspense>
