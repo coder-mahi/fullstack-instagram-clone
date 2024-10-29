aece1ce1ae487c99e5a2fda27630f74a3849b5d9
MAHESH <maheshshinde9100@gmail.com>
2024-10-29 22:25:02 +0530

    38 getPhotos in console

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
 
 const Timeline = () => {
+  const { photos } = usePhotos();
+  console.log('photos',photos);
   return (
     <div className='container col-span-2'>i am Timeline</div>
   )
