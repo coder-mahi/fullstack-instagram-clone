3408853577b266820412c6bf97130e3e8ceeb797
MAHESH <maheshshinde9100@gmail.com>
2024-10-29 12:43:58 +0530

    32 suggestions.js

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import PropTypes from 'prop-types'
+import Skeleton from 'react-loading-skeleton';
+import {getSuggestedProfiles} from "../../services/firebase";
 
-const Suggestions = () => {
-  return (
-    <div>i am suggestions</div>
-  )
-}
+const Suggestions = ({userId}) => {
+  const [profiles,setProfiles] = useState(null)
+  
+  useEffect(()=>{
+    async function suggestedProfiles(){
+      const response = await getSuggestedProfiles(userId);
+      setProfiles(response);
+    }
+    console.log('userId',userId);
+    suggestedProfiles();
+  },[userId]);
+  
+  return !profiles ? (
+    <Skeleton count={1} height={150}/>
+    ) : profiles.length>0 ? (
+<div className="rounded flex flex-col">
+  <div className="text-sm flex items-center justify-between align-items">
+    <p className="font-bold text-gray-base">Suggestions for you</p>
+  </div>
+</div>
+    ) : null;
+};
 
 export default Suggestions
+Suggestions.propTypes={
+  userId:PropTypes.string,
+};
