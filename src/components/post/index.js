52b0fa11b67974371ab88b91ea26222fa3650c01
MAHESH <maheshshinde9100@gmail.com>
2024-10-30 09:59:31 +0530

    42 Header component for Posts

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -11,25 +11,24 @@ export default function Post({ content }) {
   const handleFocus = () => commentInput.current.focus();
   console.log('content',content)
   return (
-    // <div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
-    //   <Header username={content.username} />
-    //   <Image src={content.imageSrc} caption={content.caption} />
-    //   <Actions
-    //     docId={content.docId}
-    //     totalLikes={content.likes.length}
-    //     likedPhoto={content.userLikedPhoto}
-    //     handleFocus={handleFocus}
-    //   />
-    //   <Footer caption={content.caption} username={content.username} />
-    //   <Comments
-    //     docId={content.docId}
-    //     comments={content.comments}
-    //     posted={content.dateCreated}
-    //     commentInput={commentInput}
-    //   />
-    // </div>
-
-    <p>Post Component</p>
+    <div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
+      <Header username={content.username} />
+   {/* <Image src={content.imageSrc} caption={content.caption} />
+       <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+        
+      <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      /> */}
+    </div>
   );
 }
 
