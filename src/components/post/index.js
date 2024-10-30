89ea5d9308acb7e006f8c2a767861c1f9ea0dabf
MAHESH <maheshshinde9100@gmail.com>
2024-10-30 10:02:34 +0530

    43 Image component for Post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -13,14 +13,14 @@ export default function Post({ content }) {
   return (
     <div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
       <Header username={content.username} />
-   {/* <Image src={content.imageSrc} caption={content.caption} />
+    <Image src={content.imageSrc} caption={content.caption} />
        <Actions
         docId={content.docId}
         totalLikes={content.likes.length}
         likedPhoto={content.userLikedPhoto}
         handleFocus={handleFocus}
       />
-        
+      {/*  
       <Footer caption={content.caption} username={content.username} />
       <Comments
         docId={content.docId}
